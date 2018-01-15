fb40a04f5b11adfdb13d1abbe86450d5a83d0761
Fridolin Pokorny <fridolin@redhat.com>
2018-01-15 10:27:27 +0100

    First implementation of pip handler

--- thoth_pkgdeps/handlers/pip3.py
+++ thoth_pkgdeps/handlers/pip3.py
@@ -1,30 +1,95 @@
-import re
-
 import attr
+import logging
+import re
+import typing
 
 from .base import HandlerBase
 
-_RE_DEPENDENCY = re.compile(r'Collecting ([a-zA-Z_\-.():/0-9>=<;" ]+) \(from ([a-zA-Z_\-.():/0-9>=< ]+)\)')
+_RE_COLLECTING_DEPENDENCY = re.compile(r'Collecting ([+a-zA-Z_\-.():/0-9>=<;"]+)')
+_RE_COLLECTING_DEPENDENCY_FROM = re.compile(r'Collecting ([+a-zA-Z_\-.():/0-9>=<;"]+) '
+                                            r'\(from ([a-zA-Z_\-.():/0-9>=< ]+)\)')
+
+_LOG = logging.getLogger(__name__)
 
 
 @attr.s
 class PIP3(HandlerBase):
     """Handle extracting packages from build logs - pip3 installer."""
 
-    def run(self, input_text: str) -> dict:
+    @staticmethod
+    def _do_parse_package(package_specifier: str) -> typing.Tuple[str, typing.Optional[str], typing.Optional[str]]:
+        """Parse packages from a report line and return them in a tuple describing also version, version specifier."""
+        if package_specifier.startswith('git+'):
+            _LOG.warning("Detected installing a Python package from a git repository: %r", package_specifier)
+            package_name = package_specifier
+            version = 'master'
+
+            # Try to find branch or commit specification.
+            split_result = package_specifier.rsplit('@', maxsplit=1)
+            if len(split_result) == 2:
+                package_name = split_result[0]
+                version = split_result[1]
+
+            return package_name, None, version
+
+        # See https://www.python.org/dev/peps/pep-0440/#version-specifiers for all possible values
+        for ver_spec in ('~=', '!=', '===', '==', '<=', '>=', '>', '<'):
+            split_result = package_specifier.rsplit(ver_spec, maxsplit=1)
+            if len(split_result) == 2:
+                package_name = split_result[0]
+                version = split_result[1]
+                return package_name, ver_spec, version
+
+        return package_specifier, None, None
+
+    @classmethod
+    def _parse_package(cls, package_specifier: str) -> dict:
+        """Parse packages and return them in a dictionary."""
+        result = cls._do_parse_package(package_specifier)
+        return {
+            'package': result[0],
+            'version_specifier': result[1],
+            'version': result[2]
+        }
+
+    @staticmethod
+    def _check_entry(result, package_name, package_version):
+        """Check parsed entries against reported installed entries by pip after successful installation."""
+        matched = [entry for entry in result if entry['package'] == package_name]
+
+        if len(matched) > 1:
+            _LOG.warning('Package %r was installed multiple times in versions %s',
+                         tuple(entry['version'] for entry in matched))
+
+        if package_version not in (entry['version'] for entry in matched):
+            _LOG.info('Installation of Python package %r using pip with version specifiers %s installed version %s',
+                      [(entry['version_specifier'], entry['version']) for entry in matched], package_version)
+
+    def run(self, input_text: str) -> list:
         """Find and parse installed packages and their versions from a build log."""
         result = []
         for line in input_text.split('\n'):
-            match_result = _RE_DEPENDENCY.fullmatch(line)
+            match_result = _RE_COLLECTING_DEPENDENCY_FROM.fullmatch(line)
             if match_result:
-                # TODO: implement parsing
-                result.append((match_result.group(1), match_result.group(2)))
+                dependency = self._parse_package(match_result.group(1))
+                dependency['from'] = self._parse_package(match_result.group(2))
+                result.append(dependency)
+                continue
 
-            # TODO: what about something like:
-            # pip3 install git+https://github.com/fridex/thoth-pkgdeps@master
-            # Collecting git+https://github.com/fridex/thoth-pkgdeps@master
+            match_result = _RE_COLLECTING_DEPENDENCY.fullmatch(line)
+            if match_result:
+                dependency = self._parse_package(match_result.group(1))
+                dependency['from'] = None
+                result.append(dependency)
+                continue
 
-        # TODO: check against 'Successfully installed' line in pip3 output
+            if line.startswith('Successfully installed '):
+                packages = line[len('Successfully installed '):].split(' ')
+                for package in packages:
+                    package_name, version = package.rsplit('-', maxsplit=1)
+                    self._ceck_entry(result, package_name, version)
+                continue
+
         return result
 
 
