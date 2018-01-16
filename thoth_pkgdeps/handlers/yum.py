0c739e589aa70b46e43ebf2e01192812ccd882f5
Fridolin Pokorny <fridolin@redhat.com>
2018-01-16 10:07:59 +0100

    Improve parsing yum table

--- thoth_pkgdeps/handlers/yum.py
+++ thoth_pkgdeps/handlers/yum.py
@@ -20,22 +20,47 @@ class YUM(HandlerBase):
     """Handle extracting packages from build logs - yum installer."""
 
     @staticmethod
-    def _parse_yum_table(lines: typing.List[str], start_index: int) -> typing.Tuple[list, int]:
+    def _parse_yum_table_heading(lines: typing.List[str], start_index: int) -> int:
+        """Parse yum table heading, return increment for which there should be done proceeding."""
+        header_items = ['Package', 'Arch', 'Version', 'Repository', 'Size']
+        heading = [item for item in lines[start_index].split(' ') if item]
+        if heading != header_items:
+            # yum can break table into multiple lines for some reason - this can be inconsistent
+            # regardless of terminal size in a single run
+            second_line = [item for item in lines[start_index + 1].split(' ') if item]
+            heading.extend(second_line)
+            if heading != header_items:
+                return 0
+
+            return 2
+
+        return 1
+
+    @classmethod
+    def _parse_yum_table(cls, lines: typing.List[str], start_index: int) -> typing.Tuple[list, int]:
         """Parse installed packages from a table reported by yum installer."""
-        heading = [item for item in lines[start_index + 1].split(' ') if item]
-        if heading != ['Package', 'Arch', 'Version', 'Repository', 'Size']:
+        table_offset = cls._parse_yum_table_heading(lines, start_index + 1)
+        if not table_offset:
             _LOG.debug("Unable to parse heading for yum table, skipping (line was: %r)", lines[start_index + 1])
             return [], 0
-        if lines[start_index + 2] != '='*80:
-            _LOG.debug("Unable to find table start, giving up (line was: %r)", lines[start_index + 2])
+
+        start_index += table_offset
+
+        if lines[start_index + 1] != '='*80:
+            _LOG.debug("Unable to find table start, giving up (line was: %r)", lines[start_index + 1])
             return [], 0
 
-        if not lines[start_index + 3].startswith('Installing:'):
+        if lines[start_index + 2].startswith('Installing:'):
+            upgrading = False
+        elif lines[start_index + 2].startswith('Upgrading:'):
+            upgrading = True
+        else:
             _LOG.debug("Unable to find table start with 'Installing', giving up (line was: %r)", lines[start_index + 2])
             return [], 0
 
         reported_packages = []
-        index_increment = start_index + 4
+        index_increment = start_index + 3
+        is_dependency = False
         while index_increment < len(lines):
             items = [item for item in lines[index_increment].split(' ') if item]
             if lines[index_increment].startswith(' ') and len(items) == 6:
@@ -53,9 +78,15 @@ class YUM(HandlerBase):
                     'version': version,
                     'epoch': epoch,
                     'repository': items[3],
-                    'size': "".join(items[4:])
+                    'size': "".join(items[4:]),
+                    'dependency': is_dependency,
+                    'upgrading': upgrading
                 })
                 _LOG.debug("Found installed package report: %s", reported_packages[-1])
+            elif lines[index_increment].startswith('Installing dependencies:'):
+                if is_dependency:
+                    _LOG.warning("Dependency listing heading was already present in the output, seen again")
+                is_dependency = True
             elif lines[index_increment] == '='*80:
                 _LOG.debug("Found table ending, finished installed packages parsing")
                 index_increment += 1
@@ -82,7 +113,7 @@ class YUM(HandlerBase):
             if line.startswith('='*80):
                 installed_packages, table_end_idx = self._parse_yum_table(lines, index)
                 result.extend(installed_packages)
-                index += table_end_idx
+                index += table_end_idx or 1  # on errors move to the next line
             else:
                 index += 1
 
