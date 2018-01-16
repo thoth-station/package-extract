1f3e6769d31c66bcf7dd0186c79f3c0eeed16b34
Fridolin Pokorny <fridolin@redhat.com>
2018-01-16 06:34:38 +0100

    Add pip handler for now

---
+++ test/handlers/test_pip.py
@@ -0,0 +1,10 @@
+"""Test pip package installation extraction handler."""
+import pytest
+
+
+class TestPIP:
+    """Test pip package installation extraction handler."""
+
+    @pytest.mark.skip(reason="Currently pip handler is substituted with pip3 handler")
+    def test_pip(self):
+        pass
