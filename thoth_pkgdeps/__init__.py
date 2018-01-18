fb87084057cf3458fb50491a241af2a75eee9b3b
Fridolin Pokorny <fridolin@redhat.com>
2018-01-18 11:50:26 +0100

    Expose extract_image from package

--- thoth_pkgdeps/__init__.py
+++ thoth_pkgdeps/__init__.py
@@ -1,6 +1,7 @@
 """Extraction of installed packages for project Thoth."""
 
 from .core import extract_buildlog
+from .core import extract_image
 
 __version__ = '1.0.0rc1'
 __title__ = 'thoth_pkgdeps'
