fd18ea6edf170202b8b86a113478ef610b480b1d
Christoph Görn <goern@redhat.com>
2018-09-11 16:03:29 +0200

    fixed license and coala
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/package_extract/__init__.py
+++ thoth/package_extract/__init__.py
@@ -3,8 +3,8 @@
 from .core import extract_buildlog
 from .core import extract_image
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __title__ = 'thoth-package-extract'
 __author__ = 'Fridolin Pokorny'
-__license__ = 'ASL 2.0'
+__license__ = 'GPLv3+'
 __copyright__ = 'Copyright 2018 Fridolin Pokorny'
