802f6eefe1fbb767eb9008b1a0590ba3c0dea7df
Christoph Görn <goern@redhat.com>
2018-11-08 08:38:45 +0100

    version includes dependencies versions
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/package_extract/__init__.py
+++ thoth/package_extract/__init__.py
@@ -3,7 +3,11 @@
 from .core import extract_buildlog
 from .core import extract_image
 
-__version__ = '1.0.1'
+from thoth.common import __version__ as __common__version__
+from thoth.analyzer import __version__ as __analyzer__version__
+
+__version__ = f"1.0.1+common.{__common__version__}.analyzer.{__analyzer__version__}"
+
 __title__ = 'thoth-package-extract'
 __author__ = 'Fridolin Pokorny'
 __license__ = 'GPLv3+'
