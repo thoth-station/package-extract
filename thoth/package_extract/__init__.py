71dd5042d9ff7dd72cf47b6f3dc4e9c5e91629a0
Kebechet <noreply+kebechet@redhat.com>
2021-01-28 10:05:00 +0000

    Release of version 1.1.0

--- thoth/package_extract/__init__.py
+++ thoth/package_extract/__init__.py
@@ -3,7 +3,7 @@
 from .core import extract_buildlog  # noqa F401
 from .core import extract_image  # noqa F401
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __title__ = "thoth-package-extract"
 __author__ = "Fridolin Pokorny"
 __license__ = "GPLv3+"
